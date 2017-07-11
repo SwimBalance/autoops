853f45a58c2ddb204ae1e7bf6ab40e8aa192dd61
SwimBalance <631432976@qq.com>
2017-07-11 14:05:22 +0800

    1.简化首页访问： 由autoops/index 变成 index 2.js动态加载workpage 放弃原有display的方式

--- autoops/urls.py
+++ autoops/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^autoops/', include('maintenance.urls',namespace='APP'))
+    url(r'', include('maintenance.urls',namespace='APP'))
 ]
---
+++ maintenance/static/maintenance/js/show_workpage.js
@@ -0,0 +1,87 @@
+/**
+ * Created by d046532 on 2017/7/11.
+ */
+
+$(function () {
+    $("ul li").click(function () {
+        <!-- 导入workPage-->
+        if (this.id == 'toms') {
+            $("#workpage").empty().load("./../../static/maintenance/html/workpage.html #tom_workpage");
+            $.ajax({
+                type: "GET",
+                url: "./../tomcatData/",
+                datatype: 'json',
+                success: function (datas) {
+                    var text = $('.text');
+                    text.empty();
+                    var html = '';
+                    for (var i = 0; i < datas.length; i++) {
+                        var id = datas[i]['id'];
+                        var ip = datas[i]['ipaddress'];
+                        var host = datas[i]['machine'];
+                        var dec = datas[i]['description'];
+                        html += '<tr>';
+                        html += '<td>' + id + '</td>';
+                        html += '<td>' + ip + '</td>';
+                        html += '<td>' + host + '</td>';
+                        html += '<td>' + dec + '</td>';
+                        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="check_tomcat" class="btn btn-default">';
+                        html += '<span class="glyphicon glyphicon-check" aria-hidden="true"></span></button></td>';
+                        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="start_tomcat" class="btn btn-default">';
+                        html += '<span class="glyphicon glyphicon-play" aria-hidden="true"></span></button></td>';
+                        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="stop_tomcat" class="btn btn-default">';
+                        html += '<span class="glyphicon glyphicon-stop" aria-hidden="true"></span></button></td>';
+                        html += '</tr>';
+                    }
+                    text.append(html);
+                },
+                error: function (datas) {
+                    text.append(datas);
+                }
+            });
+        } else if (this.id == 'oras') {
+            $("#workpage").empty().load("./../../static/maintenance/html/workpage.html #ora_workpage");
+            $.ajax({
+                type: "GET",
+                url: "./../oracleData/",
+                datatype: 'json',
+                success: function (datas) {
+                    var text = $('.text');
+                    text.empty();
+                    var html = '';
+                    for (var i = 0; i < datas.length; i++) {
+                        var id = datas[i]['message'];
+                        var ip = datas[i]['message'];
+                        var host = datas[i]['message'];
+                        var dec = datas[i]['message'];
+                        html += '<tr>';
+                        html += '<td>' + id + '</td>';
+                        html += '<td>' + ip + '</td>';
+                        html += '<td>' + host + '</td>';
+                        html += '<td>' + dec + '</td>';
+                        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="check_tomcat" class="btn btn-default">';
+                        html += '<span class="glyphicon glyphicon-align-left" aria-hidden="true">' + '</span>';
+                        html += '</button>' + '</td>';
+                        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="start_tomcat">start</button>' + '</td>';
+                        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="stop_tomcat">stop</button>' + '</td>';
+                        html += '</tr>';
+                    }
+                    text.append(html);
+                },
+                error: function (datas) {
+                    text.append(datas);
+                }
+            })
+        }
+    });
+});
+
+
+function opt_tomcat(obj) {
+    var id = obj.id;
+    var action = obj.name;
+    $.getJSON("./../operation", {'id': id, 'action': action}, function (data) {
+        $("#tomcat_mes").empty().append(data['message']);
+    })
+}
+
