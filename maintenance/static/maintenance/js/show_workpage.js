445ea73c4aa0d61272a0d14b24bda51c2ea1871c
SwimBalance <631432976@qq.com>
2017-07-13 11:46:09 +0800

    function loadtomcatdata

--- maintenance/static/maintenance/js/show_workpage.js
+++ maintenance/static/maintenance/js/show_workpage.js
@@ -3,7 +3,7 @@
  */
 
 $(function () {
-    $("ul li[role]").click(function () {
+    $("ul[id='ul_navigation'] li").click(function () {
         <!-- 导入workPage-->
         if (this.id == 'toms') {
             $("#workpage").empty().load("./../../static/maintenance/html/workpage.html #tom_workpage");
@@ -13,28 +13,7 @@ $(function () {
                 datatype: 'json',
                 data: {page: 1},
                 success: function (datas) {
-                    var text = $('.text');
-                    text.empty();
-                    var html = '';
-                    for (var i = 0; i < datas.length; i++) {
-                        var id = datas[i]['id'];
-                        var ip = datas[i]['ipaddress'];
-                        var host = datas[i]['machine'];
-                        var dec = datas[i]['description'];
-                        html += '<tr>';
-                        html += '<td>' + id + '</td>';
-                        html += '<td>' + ip + '</td>';
-                        html += '<td>' + host + '</td>';
-                        html += '<td>' + dec + '</td>';
-                        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="check_tomcat" class="btn btn-default">';
-                        html += '<span class="glyphicon glyphicon-check" aria-hidden="true"></span></button></td>';
-                        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="start_tomcat" class="btn btn-default">';
-                        html += '<span class="glyphicon glyphicon-play" aria-hidden="true"></span></button></td>';
-                        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="stop_tomcat" class="btn btn-default">';
-                        html += '<span class="glyphicon glyphicon-stop" aria-hidden="true"></span></button></td>';
-                        html += '</tr>';
-                    }
-                    text.append(html);
+                    loadtomcatdata(datas)
                 }
             });
         } else if (this.id == 'oras') {
@@ -44,26 +23,7 @@ $(function () {
                 url: "./../oracleData/",
                 datatype: 'json',
                 success: function (datas) {
-                    var html = '';
-                    for (var i = 0; i < datas.length; i++) {
-                        var id = datas[i]['message'];
-                        var ip = datas[i]['message'];
-                        var host = datas[i]['message'];
-                        var dec = datas[i]['message'];
-                        html += '<tr>';
-                        html += '<td>' + id + '</td>';
-                        html += '<td>' + ip + '</td>';
-                        html += '<td>' + host + '</td>';
-                        html += '<td>' + dec + '</td>';
-                        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="check_tomcat" class="btn btn-default">';
-                        html += '<span class="glyphicon glyphicon-align-left" aria-hidden="true">' + '</span>';
-                        html += '</button>' + '</td>';
-                        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="start_tomcat">start</button>' + '</td>';
-                        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="stop_tomcat">stop</button>' + '</td>';
-                        html += '</tr>';
-                    }
-                    var text = $('.text');
-                    text.empty().append(html);
+                    loadoracledata(datas)
                 }
             })
         }
@@ -92,29 +52,55 @@ function page(obj) {
         datatype: 'json',
         data: {page: page_number},
         success: function (datas) {
-            var text = $('.text');
-            text.empty();
-            var html = '';
-            for (var i = 0; i < datas.length; i++) {
-                var id = datas[i]['id'];
-                var ip = datas[i]['ipaddress'];
-                var host = datas[i]['machine'];
-                var dec = datas[i]['description'];
-                html += '<tr>';
-                html += '<td>' + id + '</td>';
-                html += '<td>' + ip + '</td>';
-                html += '<td>' + host + '</td>';
-                html += '<td>' + dec + '</td>';
-                html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="check_tomcat" class="btn btn-default">';
-                html += '<span class="glyphicon glyphicon-check" aria-hidden="true"></span></button></td>';
-                html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="start_tomcat" class="btn btn-default">';
-                html += '<span class="glyphicon glyphicon-play" aria-hidden="true"></span></button></td>';
-                html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="stop_tomcat" class="btn btn-default">';
-                html += '<span class="glyphicon glyphicon-stop" aria-hidden="true"></span></button></td>';
-                html += '</tr>';
-            }
-            text.append(html);
+            loadtomcatdata(datas)
         }
     });
 }
 
+function loadtomcatdata(datas) {
+    var text = $('.text');
+    text.empty();
+    var html = '';
+    for (var i = 0; i < datas.length; i++) {
+        var id = datas[i]['id'];
+        var ip = datas[i]['ipaddress'];
+        var host = datas[i]['machine'];
+        var dec = datas[i]['description'];
+        html += '<tr>';
+        html += '<td>' + id + '</td>';
+        html += '<td>' + ip + '</td>';
+        html += '<td>' + host + '</td>';
+        html += '<td>' + dec + '</td>';
+        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="check_tomcat" class="btn btn-default">';
+        html += '<span class="glyphicon glyphicon-check" aria-hidden="true"></span></button></td>';
+        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="start_tomcat" class="btn btn-default">';
+        html += '<span class="glyphicon glyphicon-play" aria-hidden="true"></span></button></td>';
+        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="stop_tomcat" class="btn btn-default">';
+        html += '<span class="glyphicon glyphicon-stop" aria-hidden="true"></span></button></td>';
+        html += '</tr>';
+    }
+    text.append(html);
+}
+
+function loadoracledata(datas) {
+    var html = '';
+    for (var i = 0; i < datas.length; i++) {
+        var id = datas[i]['message'];
+        var ip = datas[i]['message'];
+        var host = datas[i]['message'];
+        var dec = datas[i]['message'];
+        html += '<tr>';
+        html += '<td>' + id + '</td>';
+        html += '<td>' + ip + '</td>';
+        html += '<td>' + host + '</td>';
+        html += '<td>' + dec + '</td>';
+        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="check_tomcat" class="btn btn-default">';
+        html += '<span class="glyphicon glyphicon-align-left" aria-hidden="true">' + '</span>';
+        html += '</button>' + '</td>';
+        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="start_tomcat">start</button>' + '</td>';
+        html += '<td>' + '<button id=' + id + ' onclick="opt_tomcat(this)" name="stop_tomcat">stop</button>' + '</td>';
+        html += '</tr>';
+    }
+    var text = $('.text');
+    text.empty().append(html);
+}
